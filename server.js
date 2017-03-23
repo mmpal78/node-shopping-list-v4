7fd33e1331055e7edae4502e5f327938f32e415f
Matt Palumbo <DukGwang@Matthews-iMac.local>
2017-03-23 07:10:37 -0400

    Finished PUT request and tested

--- server.js
+++ server.js
@@ -78,29 +78,7 @@ app.put('/shopping-list/:id', jsonParser, (req, res) => {
   res.status(204).json(updatedItem);
 });
 
-app.put('/recipes/:id', jsonParser, (req, res)=>{
-  const requiredFields = ['name', 'ingredients', 'id'];
-  for(let i = 0; i < requiredFields.length; i++){
-    const field = requiredFields[i];
-    if(!(field in req.body)){
-      const message = `Missing ${field} in request body.`;
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-  if(req.params.id !== req.body.id){
-    const message = `Request path id ${req.params.id} and request body id ${req.body.id} must match.`;
-    console.error(message);
-    return res.status(400).send(message);
-  }
-  console.log(`Updating shopping list item ${req.params.id}.`);
-  const updatedItem = Recipes.update({
-    id: req.params.id,
-    name: req.body.name,
-    ingredients: req.body.ingredients
-  });
-  res.status(204).json(updatedItem);
-});
+
 
 // when DELETE request comes in with an id in path,
 // try to delete that item from ShoppingList.
@@ -128,6 +106,30 @@ app.post('/recipes', jsonParser, (req, res) => {
   }
   const item = Recipes.create(req.body.name, req.body.ingredients);
   res.status(201).json(item);
+});
+
+app.put('/recipes/:id', jsonParser, (req, res)=>{
+  const requiredFields = ['name', 'id', 'ingredients'];
+  for(let i = 0; i < requiredFields.length; i++){
+    const field = requiredFields[i];
+    if(!(field in req.body)){
+      const message = `Missing ${field} in request body.`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if(req.params.id !== req.body.id){
+    const message = `Request path id ${req.params.id} and request body id ${req.body.id} must match.`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating shopping list item ${req.params.id}.`);
+  const updatedItem = Recipes.update({
+    id: req.params.id,
+    name: req.body.name,
+    ingredients: req.body.ingredients
+  });
+  res.status(204).json(updatedItem);
 });
 
 app.delete('/recipes/:id', (req, res) => {
